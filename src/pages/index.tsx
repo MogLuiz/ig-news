206a05d60cd35a0488ff7e0d44e29629d5b48440
Luiz Henrique <luizhjramos@outlook.com>
2021-10-09 17:14:13 -0300

    feat: dinamic page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+// Components
+import Head from "next/head";
+
+// Styles
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
   return (
-    <div className={styles.title}>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Head>
+        <title>Início | ignews</title>
+      </Head>
+      <h1 className={styles.title}>Hello world</h1>
+    </>
   );
 }
