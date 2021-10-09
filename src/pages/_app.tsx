eead1b124bbfe4a9a02d058d0a7c7e83a6a2e8c0
Luiz Henrique <luizhjramos@outlook.com>
2021-10-08 23:10:24 -0300

    feat: typing app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
