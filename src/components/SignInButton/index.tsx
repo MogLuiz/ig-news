37dedf98f9fbc8962b225128577a07defe7b6b02
Luiz Henrique <luizhjramos@outlook.com>
2021-10-10 13:27:02 -0300

    feat: style for logged in and Unlogged out button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,31 @@
 // Packages
 import React from "react";
 import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 
 // Styles
 import styles from "./styles.module.scss";
 
 const SignInButton: React.FC = () => {
   // -------------------------------------------------
+  // Constants
+  // -------------------------------------------------
+  const isUserLoggedIn = true;
+
+  // -------------------------------------------------
   // Render
   // -------------------------------------------------
   return (
     <button type="button" className={styles.sign_in_button}>
-      <FaGithub color="#eba417" />
-      Sign in with Github
+      <FaGithub color={isUserLoggedIn ? "#04d361" : "#eba417"} />
+      {isUserLoggedIn ? (
+        <>
+          <span>Luiz Henrique</span>
+          <FiX color="#737380" className={styles.close_icon} />
+        </>
+      ) : (
+        <span>Sign in with Github</span>
+      )}
     </button>
   );
 };
