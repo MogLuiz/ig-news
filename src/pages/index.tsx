6048947ebb0eb26a066cbe4e8d6dc64ab3e830e1
Luiz Henrique <luizhjramos@outlook.com>
2021-10-10 14:56:20 -0300

    feat: structure homme page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // Components
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   // -------------------------------------------------
@@ -8,9 +9,28 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ignews</title>
+        <title>Home | ignews</title>
       </Head>
-      <h1>Hello world</h1>
+
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          width="450px"
+          height="450px"
+        />
+      </main>
     </>
   );
 }
