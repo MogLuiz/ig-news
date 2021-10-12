3359f42878b1588c52139f78a0aa3389ec01c06b
Luiz Henrique <luizhjramos@outlook.com>
2021-10-12 13:01:40 -0300

    feat: next-auth provider configure

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,9 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: "read:user"
     }),
     // ...add more providers here
   ],
