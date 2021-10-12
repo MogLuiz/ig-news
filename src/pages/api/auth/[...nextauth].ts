8410c1424a69f13cc94b4ee3703f6fe63381c13a
Luiz Henrique <luizhjramos@outlook.com>
2021-10-12 18:18:37 -0300

    feat: create user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,31 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 
+// FaunaDB
+import { fauna } from "../../../services/fauna"
+import { query } from "faunadb"
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: "read:user"
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+
+      const { email } = user
+
+      await fauna.query(
+        query.Create(
+          query.Collection("users"),
+          { data: { email: user.email } }
+        )
+      )
+
+      return true
+    },
+  }
 })
