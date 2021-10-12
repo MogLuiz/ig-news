93836a84f0d2d40a4c9704ca39d0e41f13194293
Luiz Henrique <luizhjramos@outlook.com>
2021-10-12 13:17:43 -0300

    feat: use hook useSession

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,16 +4,16 @@ import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 // Functions
-import { signIn } from "next-auth/client";
+import { signIn, useSession } from "next-auth/client";
 
 // Styles
 import styles from "./styles.module.scss";
 
 const SignInButton: React.FC = () => {
   // -------------------------------------------------
-  // Constants
+  // Hooks
   // -------------------------------------------------
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
   // -------------------------------------------------
   // Render
@@ -22,10 +22,10 @@ const SignInButton: React.FC = () => {
     <button
       type="button"
       className={styles.sign_in_button}
-      onClick={!isUserLoggedIn ? () => signIn() : () => {}}
+      onClick={!session ? () => signIn("github") : () => {}}
     >
-      <FaGithub color={isUserLoggedIn ? "#04d361" : "#eba417"} />
-      {isUserLoggedIn ? (
+      <FaGithub color={session ? "#04d361" : "#eba417"} />
+      {session ? (
         <>
           <span>Luiz Henrique</span>
           <FiX color="#737380" className={styles.close_icon} />
