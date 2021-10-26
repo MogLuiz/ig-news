cf2c06f2cc13ebe5f00ae63e33cfabddd146c88c
Luiz Henrique <luizhjramos@outlook.com>
2021-10-25 21:29:56 -0300

    feat: constructing handleSubscribe function

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,9 @@
 // Packages
 import React from "react";
 
+// Hooks
+import { signIn, useSession } from "next-auth/client";
+
 // Styles
 import styles from "./styles.module.scss";
 
@@ -9,8 +12,32 @@ interface ISubscribeButtonProps {
 }
 
 const SubscribeButton: React.FC<ISubscribeButtonProps> = ({ priceId }) => {
+  // -------------------------------------------------
+  // Hooks
+  // -------------------------------------------------
+
+  const [session] = useSession();
+
+  // -------------------------------------------------
+  // Functions
+  // -------------------------------------------------
+
+  const handleSubscribe = () => {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  };
+
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
   return (
-    <button type="button" className={styles.subscribe_button}>
+    <button
+      type="button"
+      className={styles.subscribe_button}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
