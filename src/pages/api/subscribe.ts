e0563352b4f31fb96edad21e6b76bd93d61a333a
Luiz Henrique <luizhjramos@outlook.com>
2021-10-25 21:48:12 -0300

    feat: subcribe function in Next API route

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { NextApiRequest, NextApiResponse} from "next"
+import { stripe } from "../../services/stripe"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if(req.method === "POST") {
+        
+        const checkoutSesssion = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            billing_address_collection: "required",
+            line_items: [
+                { price: "price_1Jj83rGD74qADWTEbxHVmfmE", quantity: 1 }
+            ],
+            mode: "subscription",
+            allow_promotion_codes: true,
+            success_url:"",
+            cancel_url: ""
+        })
+
+    } else {
+        res.setHeader("Allow", "POST")
+        res.status(405).end("Method not allowed")
+    }
+}
