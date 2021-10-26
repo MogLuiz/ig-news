80ecc5ebbf3765aac78e63d5acc3cfe4707897f0
Luiz Henrique <luizhjramos@outlook.com>
2021-10-26 17:04:56 -0300

    feat: save stripes data in Fauna database

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -3,13 +3,42 @@ import { NextApiRequest, NextApiResponse} from "next"
 import { getSession } from "next-auth/client"
 import { stripe } from "../../services/stripe"
 
+import { query as q } from 'faunadb';
+import { fauna } from "../../services/fauna";
+
+interface IUser {
+    ref: {
+        id: string;
+    }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(req.method === "POST") {
         const session = await getSession({ req })
 
+        const user = await fauna.query<IUser>(
+            q.Get(
+                q.Match(
+                    q.Index("user_by_email"),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
         const stripeCustomer = await stripe.customers.create({
             email: session.user.email,
         })
+
+        await fauna.query(
+            q.Update(
+                q.Ref(q.Collection("users"), user.ref.id),
+                {
+                    data: {
+                        stripe_customer_id: stripeCustomer.id,
+                    }
+                }
+            )
+        )
         
         const stripeCheckoutSesssion = await stripe.checkout.sessions.create({
             customer: stripeCustomer.id,
