25975479d9035f4a9a9006f789391efe3a5745d3
Luiz Henrique <luizhjramos@outlook.com>
2021-11-10 21:10:26 -0300

    feat: webhook fetch

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -7,3 +7,10 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
     res.status(200).json({ ok: true })
 }
+
+
+export (req: NextApiRequest, res: NextApiResponse) => {
+    console.log("evento recebido")
+
+    res.status(200).json({ ok: true })
+}
