f78113b51a232450ab132c3f6aff5835b2f8520d
Luiz Henrique <luizhjramos@outlook.com>
2021-11-17 17:45:37 -0300

    feat: dinamic link style when is active

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 //Packages
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 // Compontents
 import SignInButton from "../SignInButton";
@@ -9,17 +10,23 @@ import SignInButton from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export const Header: React.FC = () => {
+  const { asPath } = useRouter();
+
+  console.log(asPath);
+
   return (
     <header className={styles.header_container}>
       <div className={styles.header_content}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <Link href="/">
-            <a className={styles.active}>Home</a>
+            <a className={`${asPath === "/" ? styles.active : ""}`}>Home</a>
           </Link>
 
           <Link href="/posts" prefetch>
-            <a>Posts</a>
+            <a className={`${asPath === "/posts" ? styles.active : ""}`}>
+              Posts
+            </a>
           </Link>
         </nav>
         <SignInButton />
