69acd88d8ff5caf342013a3517b9b79d2cc60149
Luiz Henrique <luizhjramos@outlook.com>
2021-11-18 08:58:46 -0300

    feat: post page structure

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,17 +1,34 @@
+import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 
 interface IPostsProps {
-  slug: string;
-  title: string;
-  excerpt: string;
-  updatedAt: string;
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
 }
 
 export default function Post({ post }: IPostsProps) {
-  return <h1>Teste</h1>;
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main>
+        <article>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+        </article>
+      </main>
+    </>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
