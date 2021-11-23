2e8c66fa384a9102f9d1342d9993dc224aab1ffe
Luiz Henrique <luizhjramos@outlook.com>
2021-11-23 19:22:39 -0300

    test: dinamic return mocks using mocked/ts-jest

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+import { useSession } from "next-auth/client";
 import SignInButton from ".";
 
-jest.mock("next-auth/client", () => {
-  return {
-    useSession() {
-      return [null, false];
-    },
-  };
-});
+jest.mock("next-auth/client");
 
 describe("SignInButton component", () => {
   it("renders correctly when user is not authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValue([null, false]);
+
     render(<SignInButton />);
 
     expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
