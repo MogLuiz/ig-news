645a8f5ecb1af7ed6423ecb7f52f2c88b92edda6
Luiz Henrique <luizhjramos@outlook.com>
2021-11-23 21:12:13 -0300

    test: if SubscribeButton component renders correctly case test

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+import { useSession } from "next-auth/client";
+import SubscribeButton from ".";
+
+jest.mock("next-auth/client", () => {
+  return {
+    useSession() {
+      return [null, false];
+    },
+  };
+});
+
+describe("SubscribeButton component", () => {
+  it("renders correctly", () => {
+    render(<SubscribeButton />);
+
+    expect(screen.getByText("Subscribe now")).toBeInTheDocument();
+  });
+});
