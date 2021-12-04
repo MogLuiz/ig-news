4b378110fa9ba927ff61b7b4d35ccd409d3ddc51
Luiz Henrique <luizhjramos@outlook.com>
2021-12-04 19:00:36 -0300

    feat: get user in the faunaDB

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,6 +1,22 @@
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
+
 export async function saveSubscription(
     subscriptionId: string,
     customerId: string,
 ) {
     // Buscando usuário no BD com o ID { customerId }
+    
+    const userRef = await fauna.query(
+        q.Select(
+            "ref",
+            q.Get(
+                q.Match(
+                    q.Index("user_by_stripe_customer_id"),
+                    customerId
+                )
+            )
+        )
+    )
+
 }
