99e512ffb339736e156e67f7abfefa72a38ecfd9
Luiz Henrique <luizhjramos@outlook.com>
2021-12-06 20:56:21 -0300

    fix: Home page value fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ import styles from "./home.module.scss";
 interface IHomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -39,7 +39,7 @@ export default function Home({ product }: IHomeProps) {
             Get acess to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
