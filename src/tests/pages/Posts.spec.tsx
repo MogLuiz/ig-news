1219a9b433d0ad23f33e6b9190e781c0293cf2fe
Luiz Henrique <luizhjramos@outlook.com>
2022-01-10 22:04:10 -0300

    test: loads initial data testing

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { mocked } from "ts-jest/utils";
 
-import { stripe } from "../../services/stripe";
 import Posts, { getStaticProps } from "../../pages/posts";
 
 import { getPrismicClient } from "../../services/prismic";
@@ -22,5 +21,42 @@ describe("Home page", () => {
     render(<Posts posts={posts} />);
 
     expect(screen.getByText("My New Post")).toBeInTheDocument();
+  });
+
+  it("loads initial data", async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getPrismicClientMocked.mockResolvedValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: "my-new-post",
+            data: {
+              title: [{ type: "heading", text: "My new post" }],
+              content: [{ type: "paragraph", text: "Post excerpt" }],
+            },
+            last_publication_date: "04-01-2021",
+          },
+        ],
+      }),
+    });
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        // Verifica se existe no minímo essas propriedades no objeto
+        props: {
+          posts: [
+            {
+              slug: "my-new-post",
+              title: "My new post",
+              excerpt: "Post excerpt",
+              updatedAt: "01 de abril de 2021",
+            },
+          ],
+        },
+      })
+    );
   });
 });
